2afa8f55dad75c8257a999cb6ebc945fd8e2d70d
Manishakahar <manisha.kahar1998@gmail.com>
2022-01-04 23:38:29 +0530

    Added: Welcome Message

---
+++ src/main/java/com/blz/invoicegenrater/service/InvoiceService.java
@@ -0,0 +1,7 @@
+package com.blz.invoicegenrater.service;
+
+public class InvoiceService {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Cab Invoice Generator");
+    }
+}
